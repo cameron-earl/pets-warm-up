8a9627c86bd3bdf11043e4af297b19b2a0590f52
bdoring <doringbarbara@gmail.com>
2017-10-20 09:39:23 -0700

    controller and config folders

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8000;
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// require('./config/session.js')(app);
+
+app.set('view engine', 'ejs');
+
+
+// var routes_setter = require('./config/routes.js');
+// routes_setter(app);
+
+app.get('/', function (req, res) {
+  knex('pets')
+  .then((petsList) => {
+    res.render('pets', {pets: petsList})
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  res.send("hello");
+});
+
+app.post('/', function (req, res) {
+  knex('pets')
+  .insert({
+    name: req.body.name,
+    type: req.body.type,
+    gender: req.body.gender
+  }, '*')
+  .then((newPet) => {
+    console.log(newPet);
+    res.redirect('/');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+});
+
+app.listen(port, function() {
+  console.log('Listening on', port);
+});
